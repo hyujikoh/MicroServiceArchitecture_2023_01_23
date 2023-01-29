afb1bc9c94cd50a9c8a758484e1079322c2acebc
hyujikoh <dvum0045@gmail.com>
2023-01-29 23:39:37 +0900

    jhipster 기반 도서 서비스 초기 생성

---
+++ src/main/java/com/my/book/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,11 @@
+package com.my.book.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+
+}
