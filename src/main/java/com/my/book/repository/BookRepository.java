de14875e25f6910c0c3f7e36d1b294c6bcb62de8
hyujikoh <dvum0045@gmail.com>
2023-02-02 22:06:09 +0900

    book 엔티티 생성

---
+++ src/main/java/com/my/book/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.my.book.repository;
+
+import com.my.book.domain.Book;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Book entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+}
